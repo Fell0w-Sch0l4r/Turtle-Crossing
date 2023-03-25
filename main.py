c1c2757dfbc539de8a947e8d530cef38c967688b
Ângel Cagiza <angelcagiza@gmail.com>
2023-03-25 09:57:42 +0000

    add level and its functionalities on the screen

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
-#screen.tracer(0)
+screen.tracer(0)
 
 player = Player()
-
-
+score_board = Scoreboard()
 
 screen.listen()
 screen.onkey(key="Up", fun=player.up)
+
+
+while True:
+    time.sleep(0.1)
+    screen.update()
+    
+    if player.in_finish_line():
+        player.next_level()
+        score_board.increase_level()
 
 
 screen.exitonclick()
