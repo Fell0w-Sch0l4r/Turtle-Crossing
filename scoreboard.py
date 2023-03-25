6462b9f2f6922ef9f34578932a0f0863e63e687d
Ângel Cagiza <angelcagiza@gmail.com>
2023-03-25 09:56:53 +0000

    create the scoreboard class

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,30 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        self.hideturtle()
+        self.penup()
+        self.setpos(x=-290, y=260)
+        
+        self.level: int = 1
+        
+        
+        self.update_level()
+        
+        
+        
+        
+    def update_level(self):
+        self.clear()
+        self.text: str = f"Level {self.level}"
+        self.write(arg=self.text, font=FONT)
+        
+        
+    def increase_level(self):
+        self.level += 1
+        self.update_level()
