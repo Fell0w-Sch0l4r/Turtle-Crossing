9ad82fa64265d58db677760342aa4ff006bf0a00
Ângel Cagiza <angelcagiza@gmail.com>
2023-03-25 09:27:18 +0000

    create player class

--- player.py
+++ player.py
@@ -1,7 +1,19 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self, shape: str = "turtle", ) -> None:
+        super().__init__(shape)
+        
+        self.penup()
+        self.setheading(90)
+        self.setpos(STARTING_POSITION)
+        
+        
+    def up(self):
+        new_y: float = self.ycor() + MOVE_DISTANCE
+        self.sety(new_y)
